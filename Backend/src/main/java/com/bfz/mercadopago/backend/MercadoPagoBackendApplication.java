15974355830ae7a591d2fce15a0d72f9bd301341
Bruno Fernández <bruferper@gmail.com>
2022-01-25 21:28:46 -0300

    Init

---
+++ Backend/src/main/java/com/bfz/mercadopago/backend/MercadoPagoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bfz.mercadopago.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoPagoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoPagoBackendApplication.class, args);
+	}
+
+}
